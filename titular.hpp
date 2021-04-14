76bc806955e0b304f195d56572a8d557c127b847
ss <setrotec@outlook.com>
2021-04-14 00:24:38 -0300

    Aprendedo sobre composição de classes

---
+++ titular.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include<string>
+	
+class Titular{
+
+private:
+	std::string cpf_titular;
+	std::string nome_titular;
+	
+public:
+	Titular(std::string cpf_titular, std::string nome_titular);
+
+};
+	
+
+
